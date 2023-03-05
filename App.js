ce154ae031dca4f91a16d666abc3b97c1351020d
Amani-Ghanem <126444893+Amani-Ghanem@users.noreply.github.com>
2023-03-05 02:53:08 -0800

    to do list

--- App.js
+++ App.js
@@ -1,34 +1,76 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View ,FlatList, Alert, TouchableWithoutFeedback, Keyboard} from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoitem';
+import AddTodo from './components/addTodo';
+import Sandbox from './components/sandbox';
+
+
+
 
 export default function App() {
-  const [name, setName]= useState('Amani');
-  const [age, setAge]= useState('30');
+  const [todos, setTodos]= useState([
+    { text: 'Do 1' , key: '1' },
+    { text: 'Do 2' , key: '2' },
+    { text: 'Do 3' , key: '3' },
+    { text: 'Do 4' , key: '4' },
+    { text: 'Do 5' , key: '5' },
+    { text: 'Do 6' , key: '6' },
+    { text: 'Do 7' , key: '7' },
 
+    
+  ]);
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
+  const submitHandler = (text) => {
+    if(text.length >= 4){
+      setTodos ((prevTodos) => {
+        return[ 
+          {text: text, key: Math.random().toString()},
+          ...prevTodos
+        ];
   
-  
+      });
+    } else {
+      Alert.alert('Invalid Input', 'To do must be 4 or more chars long', [
+        {text: 'OK', onPress: () => console.log('alert closed')}
+      ]);
+
+
+    }
+   
+  }
+
+
 
   return (
+    // <Sandbox/>
+    <TouchableWithoutFeedback onPress={( ) => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard'); 
+    }}>
     <View style={styles.container}>
-      <Text>Enter Name: </Text>    
-     
-      <TextInput 
-      multiline
-      style={styles.input}
-      placeholder = 'e.g. Jhon Doe'
-      onChangeText={(val) => setName(val)}      
-      />
-       <Text>Enter age:  </Text>
-      <TextInput
-      multiline    
-      style={styles.input}
-      placeholder = 'e.g. 40'
-      onChangeText={(val) => setAge(val)}      
-      />
-       <Text>name: {name}, age: {age} </Text>
+      <Header/>
+      <View style = {styles.content}>
+        <AddTodo submitHandler={submitHandler}/>
       
+        <View style= {styles.list}>
+          <FlatList 
+          data = {todos} 
+          renderItem= {({item}) => (        
+           <TodoItem item= {item} pressHandler= {pressHandler} />
+          )}
+          />
 
+        </View>
+
+      </View>
+   
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -36,20 +78,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-   
-  input:{
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 10,
-    margin: 10,
-    width: 200,
     
-    
-
   },
-  
-  });
+  content:{
+    padding: 40,
+    flex: 1,
+    },
+    list:{
+      marginTop: 20,
+    },
+    item:{
+      marginTop:24,
+      padding: 5,
+      fontSize: 20,
+      marginHorizontal:10,
+      marginTop: 24,
+      
+    },
+});
